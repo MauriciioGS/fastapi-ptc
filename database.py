dcfdd5fb48070f4d54d3bac4978f3b356a0efff0
Erick Rodrigo <30943243+rodreri@users.noreply.github.com>
2022-01-04 21:12:38 -0600

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://pfhlikgnjjhcez:2c9f691a9be547f604f3059adf9993299ecd1cb7c81fbfde8451bb648a2e1bf4@ec2-35-168-80-116.compute-1.amazonaws.com:5432/df6ao8iapi959d",
+engine = create_engine("postgresql://zrngbhdjaoychd:5c219d78c07e01b2730c15f72c795b2a44a410be768db23592bb38eca8af10ab@ec2-52-200-28-255.compute-1.amazonaws.com:5432/declggmofabiin",
         echo=True
 )
 
