0548cc24e3f9ad4f98450ced6b80e71f689b4c76
H Mauricio García <30942047+MauriciioGS@users.noreply.github.com>
2023-01-11 15:03:44 -0600

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgres://admin:9xQKGUXlZjwPXh3jMKlWWfV0pfJEiyhY@dpg-cevi2j94reb4eatsep20-a.oregon-postgres.render.com/testdb_yr6w",
+engine = create_engine("postgresql://admin:9xQKGUXlZjwPXh3jMKlWWfV0pfJEiyhY@dpg-cevi2j94reb4eatsep20-a.oregon-postgres.render.com/testdb_yr6w",
         echo=True
 )
 
