6d928c4409b2795452c92d63603a5d68a6050e07
MauriciioGS <mauricaz@outlook.com>
2023-01-11 16:48:01 -0600

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 from database import Base,engine
-from models import #Announcement  # Nombre de la tabla a crear
+from models import Tool  # Nombre de la tabla a crear
 
 print("Creating database ....")
 
